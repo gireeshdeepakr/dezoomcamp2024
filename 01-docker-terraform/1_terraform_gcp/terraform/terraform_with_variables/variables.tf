8808533a2ac31f4a2320af732bcbcfabac4e72f5
root <root@gireesh-temnos-beta-01.bo3.e-dialog.com>
2024-02-03 12:57:43 -0500

    module 1 progress

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
+  default     = "~/de-2024/gcloud/dezoomcamp2024.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -8,7 +8,7 @@ variable "credentials" {
 
 variable "project" {
   description = "Project"
-  default     = "<Your Project ID>"
+  default     = "vivid-partition-412012"
 }
 
 variable "region" {
@@ -32,7 +32,7 @@ variable "bq_dataset_name" {
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "gireesh-terraform-demo-bucket"
 }
 
 variable "gcs_storage_class" {
